2073686251692390b56b0cd6bd764fa8c1698eb0
Iqbal Syifa Mahmuda <bal.mahmuda@gmail.com>
2022-09-25 11:19:30 +0700

    Changed database to sqlite for easy debugging

--- ants_silk/settings.py
+++ ants_silk/settings.py
@@ -81,22 +81,22 @@ WSGI_APPLICATION = 'ants_silk.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'silk_ddatabase',
-        'USER': 'balsemanget',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'silk_ddatabase',
+#        'USER': 'balsemanget',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
